be6f577c8f97298bfda65108927cdbcbe1c5c10a
SantiagoEguzquiza <santieguzquiza@gmail.com>
2023-06-28 19:53:25 -0300

    Funcionalidades de botones

--- WEB_LIBROS/Controllers/HomeController.cs
+++ WEB_LIBROS/Controllers/HomeController.cs
@@ -21,10 +21,39 @@ namespace WEB_LIBROS.Controllers
             API apiBooks = new API();
 
 
+            string resultado = apiBooks.getLibros("dormir+inauthor:Mario+intitle:cama", "AIzaSyAvKWpjkXwzkOCvPCqFMjQMRJkEhY3KKZI");
+
+            var listaLibros = JsonConvert.DeserializeObject<LIBROS>(resultado);
+            ViewBag.Libros = listaLibros.Items;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string autor, string titulo, string descripcion)
+        {
+            if (autor != null)
+            {
+                autor = autor.ToLower();
+                autor = "+inauthor:" + autor;
+            }else if (autor == null) { autor = ""; }
+
+            if (titulo != null)
+            {
+                titulo = titulo.ToLower();
+                titulo = "+intitle:" + titulo;
+            }else if (titulo == null) { titulo = ""; }
+
+            if (descripcion != null)
+            {
+                descripcion = descripcion.ToLower();
+            }
+            else if (descripcion == null) { descripcion = ""; }
+
+            string q = descripcion + autor + titulo;
+
+            API apiBooks = new API();
 
 
-
-            string resultado = apiBooks.getLibros("flores+inauthor:Mario", "AIzaSyAvKWpjkXwzkOCvPCqFMjQMRJkEhY3KKZI");
+            string resultado = apiBooks.getLibros(q , "AIzaSyAvKWpjkXwzkOCvPCqFMjQMRJkEhY3KKZI");
 
             var listaLibros = JsonConvert.DeserializeObject<LIBROS>(resultado);
             ViewBag.Libros = listaLibros.Items;
